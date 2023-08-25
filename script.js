70c78b6682fa8f6063936487d2bc65bdd4f70546
Ilyuz <wingeddeath62@gmail.com>
2023-08-25 18:17:47 +0300

    «комментарий»

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+'use strict';
+let lang = 'ru';
 
-const num = 266219;
-const x = num.toString().split('').reduce((a, b) => a * b);
-console.log(x);
-const y = x ** 3;
-console.log(y);
-console.log(String(y).slice(0, 2));
+if (lang === 'ru') {
+    console.log('Понедельник,Вторник,Среда,Четверг,Пятница,Суббота,Воскресенье')
+} else if (lang === 'en') {
+    console.log("Monday,Tuesday,Wednesday,Thursda,Friday,Saturday,Sunday")
+};
+lang = 'en';
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник,Вторник,Среда,Четверг,Пятница,Суббота,Воскресенье');
+        break;
+    case 'en':
+        console.log("Monday,Tuesday,Wednesday,Thursda,Friday,Saturday,Sunday");
+        break;
+}
+
+    const lang_array = [];
+    lang_array['ru'] = ['Пн', 'Вт', 'СР', 'Чт', 'Пт', 'Сб', 'Вс'];
+    lang_array['en'] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+console.log(lang_array[lang = 'en']);
+
+let namePerson = 'Артем';
+namePerson == 'Артем' ? console.log('директор') : console.log('студент') && namePerson == 'Александр' ? console.log('преподаватель') : console.log('студент'); 
 
