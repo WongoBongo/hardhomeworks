5cbe22628a71c8c18e597141a026bad0a04b1c1b
Ilyuz <wingeddeath62@gmail.com>
2023-08-26 11:22:35 +0300

    lesson4hard

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
 'use strict';
-let lang = 'ru';
 
-if (lang === 'ru') {
-    console.log('Понедельник,Вторник,Среда,Четверг,Пятница,Суббота,Воскресенье')
-} else if (lang === 'en') {
-    console.log("Monday,Tuesday,Wednesday,Thursda,Friday,Saturday,Sunday")
-};
-lang = 'en';
-switch (lang) {
-    case 'ru':
-        console.log('Понедельник,Вторник,Среда,Четверг,Пятница,Суббота,Воскресенье');
-        break;
-    case 'en':
-        console.log("Monday,Tuesday,Wednesday,Thursda,Friday,Saturday,Sunday");
-        break;
+function test(str) {
+  if (typeof str !== 'string') {
+    alert('аргумент не строка');
+    return;
+  }
+
+  str = str.trim('');
+  return str.length > 30 ? str.slice(0, 30) + '...' : str;
 }
-
-    const lang_array = [];
-    lang_array['ru'] = ['Пн', 'Вт', 'СР', 'Чт', 'Пт', 'Сб', 'Вс'];
-    lang_array['en'] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-console.log(lang_array[lang = 'en']);
-
-let namePerson = 'Артем';
-namePerson == 'Артем' ? console.log('директор') : console.log('студент') && namePerson == 'Александр' ? console.log('преподаватель') : console.log('студент'); 
 
