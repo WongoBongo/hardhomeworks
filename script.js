e249a8f5418593ab33e42928f0b81df7a6bc9e2e
Ilyuz <wingeddeath62@gmail.com>
2023-08-27 20:25:31 +0300

    исправлен

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ function test(str) {
   return str.length > 30 ? str.slice(0, 30) + '...' : str;
 }
 
+let string = "Имя пользователя";
+let stringLength = 'asdlkasjdlasjdaslkdajsldlasjdlasdjlasdkjalsdjalsdjalsdjl';
+let number = 123;
+
+test(string);
+console.log(test(string));
+test(number);
+console.log(test(number));
+test(stringLength);
+console.log(test(stringLength));
